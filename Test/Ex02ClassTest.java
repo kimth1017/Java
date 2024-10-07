7e2a1b8d95f8f1604e981ea522c33ad0d6d5925a
kimth1017 <kimth1017@gmail.com>
2024-10-07 09:03:00 +0900

    Update Ex02ClassTest.java

--- Test/Ex02ClassTest.java
+++ Test/Ex02ClassTest.java
@@ -7,7 +7,7 @@ public class Ex02ClassTest {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("도서관리 프로그램");
 		
-		Book2[] books = new Book2[10];
+		Book1[] books = new Book1[10];
 		
 		int bookCount = 0;
 		
@@ -17,9 +17,10 @@ public class Ex02ClassTest {
 			String title = "";
 			String author = "";
 			int price = 0;
-			
-			System.out.println("---------------------");
-			System.out.println("메뉴를 선택하세요. \n<1:책등록 2:책목록, 0:종료>");
+			//Book1 book = null;
+					
+			System.out.println("------------------");
+			System.out.println("메뉴를 선택하세요. <1: 책등록>, <2: 책목록>, <0: 종료>");
 			System.out.println(">>");
 			int menu = scanner.nextInt();
 			scanner.nextLine();
@@ -27,41 +28,49 @@ public class Ex02ClassTest {
 			switch(menu) {
 				case 1:
 					System.out.println("책을 등록하세요.");
-					System.out.println("제목:");
+					System.out.print("제목:");
 					title = scanner.nextLine();
-					System.out.println("저자:");
+					System.out.print("저자:");
 					author = scanner.nextLine();
-					System.out.println("가격:");
+					System.out.print("가격:");
 					price = scanner.nextInt();
-					System.out.printf("<%d> 제목: %s, 저자: %s, 가격: %d%n%n", bookCount+1, title, author, price);
+					System.out.printf("제목: %s, 저자: %s, 가격: %d%n%n", title, author, price);
+
+/*					book = new Book1();
+					book.title = title;
+					book.author = author;
+					book.price = price;
+					books[bookCount] = book;*/
 					
-					books[bookCount] = new Book2();
-					books[bookCount].title = title;
-					books[bookCount].author = author;
-					books[bookCount].price = price;
+					books[bookCount]=new Book1();
+					books[bookCount].title=title;
+					books[bookCount].author=author;
+					books[bookCount].price=price;
 					bookCount++;
 					break;
 					
 				case 2:
 					System.out.println("책의 목록을 확인합니다.");
 					for (int i = 0; i < bookCount; i++) {
-						System.out.printf("<%d> 제목: %s, 저자: %s, 가격: %d%n%n",i+1,books[i].title,books[i].author,books[i].price);
+						System.out.printf("%d. 제목: %s, 저자: %s, 가격: %d%n%n", i+1, books[i].title, books[i].author, books[i].price);
 					}
 					break;
 					
 				case 0:
 					run = false;
 					break;
+					
 			}
+			
 			if (!run)
 				break;
 		}
 		System.out.println("프로그램이 종료됩니다.");
 	}
 
-
 }
-class Book2{
+
+class Book1 {
 	String title;
 	String author;
 	int price;
